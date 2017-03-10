9eb367c348f5d0a8344a6783b3a24072e873ec9a
Besck4rk <leonard.brafman@gmail.com>
2017-03-10 20:15:09 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,11 +4,12 @@
 
 ## Ploting the total PM2.5 emission in 1999, 2002, 2005 and 2008
 png("plot1.png")
-> barplot(
-+   (aggTotals$Emissions)/10^6,
-+   names.arg=aggTotals$year,
-+   xlab="Year",
-+   ylab="PM2.5 Emissions (10^6 Tons)",
-+   main="Total PM2.5 Emissions From All US Sources"
-+ )
-> dev.off()
+barplot(
+  (aggTotals$Emissions)/10^6,
+  names.arg=aggTotals$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (10^6 Tons)",
+  main="Total PM2.5 Emissions From All US Sources")
+dev.off()
+
+## Conclusion: the total PM2.5 decrease over the years
