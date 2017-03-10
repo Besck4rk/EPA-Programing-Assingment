a83a6aa856ab849cc9656b7eeebfe69467064f6c
Besck4rk <leonard.brafman@gmail.com>
2017-03-10 20:12:29 +0100

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## Opening and reading the files
+> NEI <- readRDS("summarySCC_PM25.rds")
+> SCC <- readRDS("Source_Classification_Code.rds")
+
+## Ploting the total PM2.5 emission in 1999, 2002, 2005 and 2008
+png("plot1.png")
+> barplot(
++   (aggTotals$Emissions)/10^6,
++   names.arg=aggTotals$year,
++   xlab="Year",
++   ylab="PM2.5 Emissions (10^6 Tons)",
++   main="Total PM2.5 Emissions From All US Sources"
++ )
+> dev.off()
